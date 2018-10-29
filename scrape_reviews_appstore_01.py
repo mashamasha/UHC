ea45e87fb55f5df37abd40fb9bbf1791c69c0c84
Tomo <ttishihara@usfca.edu>
2018-10-29 11:34:27 -0700

    Added comments, minor changes

--- scrape_reviews_appstore_01.py
+++ scrape_reviews_appstore_01.py
@@ -1,9 +1,3 @@
-
-# coding: utf-8
-
-# In[4]:
-
-
 import os
 import json
 import requests
@@ -12,12 +6,10 @@ import xmltodict
 from collections import Counter
 
 
-# In[5]:
-
-
 def fetch_reviews(app_id, country = 'us', sortBy = 'mostRecent', page = 1):
     """ Get max 500 user reviews for a given app.
     
+    app_id = ID number of application.
     country = default to United States (us).
     sorty = 'mostRecent'(default) or 'mostHelpful'.
     page = page number. default = 1, max = 10.
@@ -30,13 +22,13 @@ def fetch_reviews(app_id, country = 'us', sortBy = 'mostRecent', page = 1):
     r = requests.get(url)
     reviews_dict = xmltodict.parse(r.text)
     
-    try:  # If there are no reviews on this page, break out of loop
+    try:  # If there are no reviews on this page, return empty list
         reviews_list = reviews_dict['feed']['entry']
     except:
         return []
     
     reviews = []
-    for review in reviews_list:
+    for review in reviews_list:  # Gather data from each review and put into a list.
         reviews.append({'title': review['title'],
                        'author': review['author']['name'],
                        'authorUrl': review['author']['uri'],
@@ -49,50 +41,43 @@ def fetch_reviews(app_id, country = 'us', sortBy = 'mostRecent', page = 1):
     return reviews
 
 
-# In[6]:
 
+if __name__ == "__main__":
 
-with open("data/health_appstore.json", 'r', encoding='utf-8') as f:
-    apps = json.load(f)
-    
-filtered_apps = [ app for app in apps['results'] 
-                 if app['primaryGenreName'] == 'Health & Fitness' ] # Filter out all non-Health&Fitness apps.
+    with open("data/healthfitness_appstore_200.json", 'r', encoding='utf-8') as f:
+        apps = json.load(f)
+        
+    filtered_apps = [ app for app in apps['results'] 
+                     if app['primaryGenreName'] == 'Health & Fitness' ] # Filter out all non-Health&Fitness apps.
 
+    app_metadata = []
 
-# In[7]:
+    for app in filtered_apps:
+        # Get app metadata
+        app_data = {'name': app['trackName'],
+                    'id': app['trackId'],
+                    'url': app['trackViewUrl'],
+                    'price': app['price'],
+                    'avgUserRating': app['averageUserRating'],
+                    'userRatingCount': app['userRatingCount'],
+                    'currentVersionReleaseDate': app['currentVersionReleaseDate'],
+                    'description': app['description'].replace('\n', ' ')
+                    }
+        
+        # Get most recent and most helpful reviews (separately) for app (up to 500)
+        # NOTE: If the app does not have many reviews, the same reviews may be included in both recent
+        # and helpful reviews.
+        recent_reviews = []
+        helpful_reviews = []
+        for i in range(1, 11): 
+            recent_reviews += fetch_reviews(app_data['id'], page=i)
+            helpful_reviews += fetch_reviews(app_data['id'], sortBy='mostHelpful', page=i)
 
+        app_data['recent_reviews'] = recent_reviews
+        app_data['helpful_reviews'] = helpful_reviews
+        app_metadata.append(app_data)
 
-app_metadata = []
+    with open('appstore_metadata_and_reviews_200.json', 'w', encoding='utf-8') as f:
+        f.write(json.dumps(app_metadata, ensure_ascii=False))
 
-for app in filtered_apps[40:42]:
-    # Get app metadata
-    app_data = {'name': app['trackName'],
-                'id': app['trackId'],
-                'url': app['trackViewUrl'],
-                'price': app['price'],
-                'avgUserRating': app['averageUserRating'],
-                'userRatingCount': app['userRatingCount'],
-                'currentVersionReleaseDate': app['currentVersionReleaseDate'],
-                'description': app['description'].replace('\n', ' ')
-                }
-    
-    # Get most recent and most helpful reviews (separately) for app (up to 500)
-    recent_reviews = []
-    helpful_reviews = []
-    for i in range(1, 11): 
-        recent_reviews += fetch_reviews(app_data['id'], page=i)
-        helpful_reviews += fetch_reviews(app_data['id'], sortBy='mostHelpful', page=i)
-
-    app_data['recent_reviews'] = recent_reviews
-    app_data['helpful_reviews'] = helpful_reviews
-    app_metadata.append(app_data)
-
-
-# In[9]:
-
-
-with open('appstore_metadata_and_reviews.json', 'w', encoding='utf-8') as f:
-    f.write(json.dumps(app_metadata, ensure_ascii=False))
-
-
-print("DONE!")
+    print("DONE!")
